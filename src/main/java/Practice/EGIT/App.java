b8d864a97c89c5317467359cf9e5651b9ed90f42
himalayashinde@gmail.com <himalayashinde@gmail.com>
2022-07-06 15:42:08 +0530

    my first egit commit

---
+++ src/main/java/Practice/EGIT/App.java
@@ -0,0 +1,7 @@
+package Practice.EGIT;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/Practice/EGIT/Egit.java
@@ -0,0 +1,12 @@
+package Practice.EGIT;
+
+public class Egit {
+
+	public static void main(String[] args) {
+		
+	System.out.println("Hello World Using EGIT Tool");
+	System.out.println("Himalaya created you");
+
+	}
+
+}
