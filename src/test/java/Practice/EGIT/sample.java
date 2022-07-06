620b92cc53e91fb44ae0d117a9094fac3357d2dc
himalayashinde@gmail.com <himalayashinde@gmail.com>
2022-07-06 17:35:09 +0530

    finally i know how to use EGIT

---
+++ src/test/java/Practice/EGIT/sample.java
@@ -0,0 +1,13 @@
+package Practice.EGIT;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("finally i have completed the work of EGIT");
+
+	}
+
+}
